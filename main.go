3518241c68bc989bdfa39b11763f7a1c6bace62a
thistle <bcmertz@email.wm.edu>
2020-07-27 01:01:10 -0400

    comment code and refactor

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"ray-tracer/vector"
 )
 
+// coordinate grid dimensions, pixes are mapped onto this grid we define
 var lower_left = vector.Vector{
 	X: -2.0,
 	Y: -1.0,
@@ -40,21 +41,28 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	const nx = 200.0
-	const ny = 100.0
-	for j := ny - 1; j >= 0; j-- {
-		for i := 0.0; i < nx; i++ {
-			fmt.Printf("\rOn image line %v", j)
-			u := float64(i / nx)
-			v := float64(j / ny)
+	// define dimensions of the image
+	const lines_x = 200.0
+	const lines_y = 100.0
+	// iterate through the image pixels
+	for y := lines_y - 1; y >= 0; y-- {
+		for x := 0.0; x < lines_x; x++ {
+			// sanity check
+			fmt.Printf("\rAt image coordinate %v %v", x, y)
+			// scale pixel number to x,y grid size
+			x_coord := horiz.ScalarMultiply(float64(x / lines_x))
+			y_coord := vert.ScalarMultiply(float64(y / lines_y))
+			// define the begining point of the ray and the direction vector into the image (from the camera)
 			r := ray.Ray{
 				Origin:    origin,
-				Direction: lower_left.Add(horiz.ScalarMultiply(u)).Add(vert.ScalarMultiply(v)),
+				Direction: lower_left.Add(x_coord).Add(y_coord), // from lower left hand side of the screen move up y_coord and right x_coord
 			}
 			c := colorGradient(r)
+			// convert rgb colors to 255 values from unit color values
 			var ir int = int(255 * c.R)
 			var ig int = int(255 * c.G)
 			var ib int = int(255 * c.B)
+			// write out the pixel value
 			_, err := f.WriteString(fmt.Sprintf("%v %v %v\n", ir, ig, ib))
 			if err != nil {
 				fmt.Println(err)
@@ -63,6 +71,7 @@ func main() {
 	}
 }
 
+// create linear gradient of blue and white
 func colorGradient(r ray.Ray) color.Color {
 	var direction vector.Vector = r.Direction
 	t := 0.5 * (direction.Y + 1)
@@ -80,6 +89,7 @@ func colorGradient(r ray.Ray) color.Color {
 	return c1.ScalarMultiply(1.0 - t).Add(c2.ScalarMultiply(t))
 }
 
+// write ppm file header
 func writePPMHeader(f *os.File) error {
 	const color_type = "P3"
 	const rows = 200
